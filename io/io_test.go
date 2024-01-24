aaf007324e62438763396781c5a4ea5d8f820952
Matias Rozanec <rozanecm@gmail.com>
2024-01-24 00:06:57 -0300

    feat(setup): add first tests and linter action to CI

---
+++ io/io_test.go
@@ -0,0 +1,32 @@
+package io
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestFromAdjacencyList(t *testing.T) {
+	var buffer bytes.Buffer
+	buffer.WriteString("1,2\n2,3\n3,4\n")
+	FromAdjacencyList(&buffer)
+}
+
+func TestLinetoList(t *testing.T) {
+	list := lineToList([]string{"1", "2", "3"})
+
+	if len(list) != 3 {
+		t.Errorf("Expected 3, got %d", len(list))
+	}
+
+	if list[0] != 1 {
+		t.Errorf("Expected 1, got %d", list[0])
+	}
+
+	if list[1] != 2 {
+		t.Errorf("Expected 2, got %d", list[1])
+	}
+
+	if list[2] != 3 {
+		t.Errorf("Expected 3, got %d", list[2])
+	}
+}
